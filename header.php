b7de62661a1cec3d65c1927aafbc0bd911bb2b52
theerapong-gam <68782075+theerapong-gam@users.noreply.github.com>
2022-11-02 00:38:24 -0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,34 @@
+<?php
+  include 'config.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="../css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">OGthee</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li><a href="../stock/table.php">Stock</a></li>
+      <li><a href="../order/table.php">Order</a></li>
+    </ul>
+  </div>
+</nav>
+  
+<div class="container">
+ 
+</div>
